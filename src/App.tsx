386902b759654f23abf93455928f92bd00209677
Star Dev <91700749+kanbei0605@users.noreply.github.com>
2023-04-28 10:19:43 +0900

    implement click box function and comments

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
+import ColorBox from "./components/ColorBox";
 import "./App.css";
 
 function App() {
+  const [colors, setColors] = useState<string[]>([
+    "blue",
+    "red",
+    "burlywood",
+    "brown",
+    "purple",
+    "aqua",
+    "aquamarine",
+    "violet",
+    "darkgray",
+  ]);
+
+  // Define a function to generate a random color string
+  const generateColor = (): string => {
+    let randomColorString = "#";
+    const arrayOfColorFunctions = "0123456789abcdef";
+    for (let x = 0; x < 6; x++) {
+      let index = Math.floor(Math.random() * 16);
+      let value = arrayOfColorFunctions[index];
+      randomColorString += value;
+    }
+    return randomColorString;
+  };
+
+  // Define a function to handle click events on the box
+  const onClickBox = () => {
+    const newColors = colors.map(() => generateColor());
+    setColors(newColors);
+  };
+
   return (
     <div className="container grid">
       <div className="first-fourth--container">
-        <div className="first">
-          <span>1</span>
-        </div>
+        <ColorBox color={colors[0]} text="1" onClick={onClickBox} />
         <div className="second-fourth--container">
-          <div className="second">
-            <span>2</span>
-          </div>
+          <ColorBox
+            className="second"
+            color={colors[1]}
+            text="2"
+            onClick={onClickBox}
+          />
           <div className="third-fourth--container">
-            <div className="third">
-              <span>3</span>
-            </div>
-            <div className="fourth">
-              <span>4</span>
-            </div>
+            <ColorBox color={colors[2]} text="3" onClick={onClickBox} />
+            <ColorBox color={colors[3]} text="4" onClick={onClickBox} />
           </div>
         </div>
       </div>
       <div className="fifth-nineth--container">
         <div className="fifth-sixth--container">
-          <div className="fifth">
-            <span>5</span>
-          </div>
-          <div className="sixth">
-            <span>6</span>
-          </div>
+          <ColorBox color={colors[4]} text="5" onClick={onClickBox} />
+          <ColorBox color={colors[5]} text="6" onClick={onClickBox} />
         </div>
-        <div className="seventh">
-          <span>7</span>
-        </div>
+        <ColorBox
+          className="seventh"
+          color={colors[6]}
+          text="7"
+          onClick={onClickBox}
+        />
         <div className="eighth-nineth--container">
-          <div className="eighth">
-            <span>8</span>
-          </div>
-          <div className="nineth">
-            <span>9</span>
-          </div>
+          <ColorBox color={colors[7]} text="8" onClick={onClickBox} />
+          <ColorBox color={colors[8]} text="9" onClick={onClickBox} />
         </div>
       </div>
     </div>
--- src/components/ColorBox.tsx
+++ src/components/ColorBox.tsx
@@ -1,5 +1,21 @@
-const ColorBox = () => {
-  return <div>asdf</div>;
+import "./ColorBox.css";
+export interface IColorBox {
+  className?: string;
+  color: string;
+  text: string;
+  onClick: () => void;
+}
+
+const ColorBox: React.FC<IColorBox> = (props) => {
+  return (
+    <div
+      className={`colorBox ${props.className}`}
+      style={{ backgroundColor: props.color }}
+      onClick={props.onClick}
+    >
+      <span>{props.text}</span>
+    </div>
+  );
 };
 
 export default ColorBox;
